8dc45f00cd08a9440a0ad14f23748fb9f8dcd834
vladhirko <vladon0210@gmail.com>
2023-03-09 22:10:54 +0100

    lesson_21

---
+++ JS/script.js
@@ -0,0 +1,7 @@
+let number_1 = prompt("Enter first number")
+let number_2 = prompt("Enter second number")
+
+alert(Number(number_1) + Number(number_2))
+alert(number_1 - number_2)
+alert(number_1 * number_2)
+alert(number_1 / number_2)
